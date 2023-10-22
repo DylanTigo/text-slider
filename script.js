3af4985b1677c9ac4f42a11fa1cc7fbf9fe1b39e
DylanTigomo <dylannoeltigomo@gmail.com>
2023-10-22 16:43:23 +0100

    Version finale

---
+++ script.js
@@ -0,0 +1,20 @@
+// const sliderkidS = document.querySelectorAll(".slider-1");
+
+// sliderkidS.forEach(sliderkid => sliderkid.addEventListener("mouseover", handleHover));
+// function handleHover(e) {
+//   let link
+//   if(e.target.localName !== "a"){
+//     link = e.target.parentElement
+//   }else {
+//     link = e.target
+//   }
+//   const slider = link.parentElement.parentElement
+//   for (const child of slider.children) {
+//     if(child.style.animationPlayState = "play"){
+//         child.style.animationPlayState = "paused"
+//     }else{
+//         child.style.animationPlayState = "play"
+//     }
+//   }
+//   link.style.color = "#fff"
+// }
